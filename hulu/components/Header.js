779b6526c24a83d94e291b7076544681e77fbf7c
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-05-21 13:14:35 +0300

    :zap: finished header

--- hulu/components/Header.js
+++ hulu/components/Header.js
@@ -4,9 +4,14 @@ import { HomeIcon, BadgeCheckIcon, CollectionIcon, LightningBoltIcon, SearchIcon
 
 function Header() {
   return (
-    <header className="flex">
-      <div>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title='HOME' Icon={HomeIcon}/>
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
+        <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+        <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
       </div>
       <Image 
       className="object-contain"
--- hulu/components/HeaderItem.js
+++ hulu/components/HeaderItem.js
@@ -1,8 +1,8 @@
 function HeaderItem({title, Icon}) {
   return (
-    <div>
-      <Icon />
-      <p> {title} </p>
+    <div className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce"/>
+      <p className="tracking-widest opacity-0 group-hover:opacity-100"> {title} </p>
     </div>
   )
 }
