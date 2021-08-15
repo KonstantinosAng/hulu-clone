0ce74ae05d72317bbd5f68e51dda682d8c556465
CyberBoy <44067624+KonstantinosAng@users.noreply.github.com>
2021-08-15 19:48:47 +0300

    Update Header.js

--- hulu/components/Header.js
+++ hulu/components/Header.js
@@ -10,7 +10,7 @@ function Header() {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItem title='h' Icon={HomeIcon}/>
+        <HeaderItem title='HOME' Icon={HomeIcon}/>
         <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
         <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
         <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
