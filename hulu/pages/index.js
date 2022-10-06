5074af765059c2c931113d98866c8041aba861b9
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2022-10-06 23:11:37 +0300

    feat(readme): - update readme

--- hulu/pages/index.js
+++ hulu/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import Results from '../components/Results';
